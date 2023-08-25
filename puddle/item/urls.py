bbca36482bc65b31669d5399a9073957af50081d
buraksaler <burakksaler@gmail.com>
2023-08-25 04:17:36 +0300

    add browsing

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
+  path('', views.items, name='items'),
   path('new/', views.new, name='new'),
   path('<int:item_id>/', views.detail, name='detail'),
   path('<int:item_id>/delete/', views.delete, name='delete'),
   path('<int:item_id>/edit/', views.edit, name='edit'),
+  
 ]
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
+
+def items(request):
+  query = request.GET.get('query','')
+  items = Item.objects.filter(is_sold=False)
+  categories = Category.objects.all()
+  category_id = request.GET.get('category', 0)
+  
+  if category_id:
+    items = items.filter(category_id=category_id) 
+  
+  if query:
+    items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+  
+  return render(request, 'item/items.html', {'items': items, 'query': query, 'categories': categories, 'category_id': int(category_id)})
 
 def detail(request, item_id): 
   item = get_object_or_404(Item, pk=item_id)
