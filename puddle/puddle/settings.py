141f14727dcfa860aa4c1c85ca0abf26db3d54c2
buraksaler <burakksaler@gmail.com>
2023-09-07 02:47:28 +0300

    trying to deploy postgres to heroku

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,10 +85,10 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'puddle',
-        'USER': 'postgres',
-        'PASSWORD': 'apt-get',
-        'HOST': 'localhost',
+        'NAME': 'd9ak8bon3jsbrp',
+        'USER': 'hfsskgtpxzyjre',
+        'PASSWORD': '9bcdbeb17c48a30c99efbc1a83b75cca4747fef08b2858b45625e121b0f3050d',
+        'HOST': 'ec2-52-215-68-14.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -129,6 +131,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
